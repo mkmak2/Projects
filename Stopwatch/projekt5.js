22da3b35bf9ea9f0908f6eb4e4770b68987f043b
Michał Kmak <mkmak.code@gmail.com>
2022-09-07 18:46:10 +0200

    interwal change

--- Stopwatch/projekt5.js
+++ Stopwatch/projekt5.js
@@ -1,22 +1,20 @@
-const btnStart = document.querySelector(".start");
-const btnPause = document.querySelector(".pause");
-const btnStop = document.querySelector(".stop");
-const btnClear = document.querySelector(".clear");
-const btnArchiv = document.querySelector(".archiv");
-const stopwatch = document.querySelector(".stopwatch");
-const time = document.querySelector(".time");
-const timeList = document.querySelector(".timelist");
+const btnStart = document.querySelector('.start');
+const btnPause = document.querySelector('.pause');
+const btnStop = document.querySelector('.stop');
+const btnClear = document.querySelector('.clear');
+const btnArchiv = document.querySelector('.archiv');
+const stopwatch = document.querySelector('.stopwatch');
+const time = document.querySelector('.time');
+const timeList = document.querySelector('.timelist');
 
-const btnInfo = document.querySelector(".info");
-const modalShadow = document.querySelector(".modal-shadow");
-const btnCloseModal = document.querySelector(".close");
+const btnInfo = document.querySelector('.info');
+const modalShadow = document.querySelector('.modal-shadow');
+const btnCloseModal = document.querySelector('.close');
 
-const btnTheme = document.querySelector(".theme");
-const themePalette = document.querySelector(".theme-palette");
+const btnTheme = document.querySelector('.theme');
+const themePalette = document.querySelector('.theme-palette');
 const themeList = themePalette.querySelectorAll('p');
 let root = document.documentElement;
-
-
 
 let timeCounter;
 let seconds = 0;
@@ -39,7 +37,7 @@ const startStoper = () => {
 		} else {
 			stopwatch.textContent = `${minutes}:${seconds}`;
 		}
-	}, 100);
+	}, 1000);
 };
 //PAUSE BUTTON FUNCTION
 const pauseStoper = () => {
@@ -48,85 +46,80 @@ const pauseStoper = () => {
 
 //STOP BUTTON FUNCTION
 const stopStoper = () => {
-
-	if (stopwatch.textContent !== "0:00") {
+	if (stopwatch.textContent !== '0:00') {
 		time.innerHTML = `<span>Ostatni czas:</span> ${stopwatch.textContent}`;
-		time.style.visibility = "visible";
+		time.style.visibility = 'visible';
 		timesArr.push(stopwatch.textContent);
 	}
-    clearStuff();
+	clearStuff();
 };
-
 
 //CLEAR FUNCTIONS
 const clearStuff = () => {
-    clearInterval(timeCounter);
-    seconds = 0;
-    minutes = 0;
-    timeList.textContent = '';
-    timeList.style.display = 'none';
-    stopwatch.textContent = '0:00';
-
-}
+	clearInterval(timeCounter);
+	seconds = 0;
+	minutes = 0;
+	timeList.textContent = '';
+	timeList.style.display = 'none';
+	stopwatch.textContent = '0:00';
+};
 const clearStoper = () => {
-    clearStuff();
-    time.textContent = '';
-    time.style.visibility = "hidden";
-    timesArr = [];
-}
+	clearStuff();
+	time.textContent = '';
+	time.style.visibility = 'hidden';
+	timesArr = [];
+};
 
 //SHOWING TIMES LISTS
 const showArchiv = () => {
-
-    if(timeList.style.display === 'none'){
-        timeList.style.display = 'block';
-        timesArr.forEach(time => {
-            const newTime = document.createElement('li');
-            newTime.innerHTML = `Pomiar nr ${timesArr.indexOf(time)+1}: <span>${time}</span> `;
-            timeList.appendChild(newTime);
-        })
-    } else {
-        const timeListElements = document.querySelectorAll('li');
-        timeListElements.forEach(time => {
-            time.remove();
-        })
-        timeList.style.display = 'none';
-    }
+	if (timeList.style.display === 'none') {
+		timeList.style.display = 'block';
+		timesArr.forEach(time => {
+			const newTime = document.createElement('li');
+			newTime.innerHTML = `Pomiar nr ${
+				timesArr.indexOf(time) + 1
+			}: <span>${time}</span> `;
+			timeList.appendChild(newTime);
+		});
+	} else {
+		const timeListElements = document.querySelectorAll('li');
+		timeListElements.forEach(time => {
+			time.remove();
+		});
+		timeList.style.display = 'none';
+	}
 };
 
 //SHOW/HIDE INFO FUNCTIONS
-const showInfo = () =>{
-    modalShadow.style.display ='block';
-}
+const showInfo = () => {
+	modalShadow.style.display = 'block';
+};
 
 const hideInfo = () => {
-    modalShadow.style.display ='none';
-}
+	modalShadow.style.display = 'none';
+};
 
 //THEME FUNCTIONS:
 const themePanelDisplay = () => {
-    if(themePalette.style.display === 'none'){
-        themePalette.style.display = 'block';
-    } else {
-        themePalette.style.display = 'none';
-    }
-}
+	if (themePalette.style.display === 'none') {
+		themePalette.style.display = 'block';
+	} else {
+		themePalette.style.display = 'none';
+	}
+};
 const changeColor = e => {
-    root.style.setProperty('--first-color', `${e.style.color}`);
-}
+	root.style.setProperty('--first-color', `${e.style.color}`);
+};
 themeList.forEach(color => {
-    color.addEventListener('click', changeColor(color));
-})
-
-
+	color.addEventListener('click', changeColor(color));
+});
 
 //BUTTON ACTIONS
-btnStart.addEventListener("click", startStoper);
-btnPause.addEventListener("click", pauseStoper);
-btnStop.addEventListener("click", stopStoper);
-btnArchiv.addEventListener("click", showArchiv);
+btnStart.addEventListener('click', startStoper);
+btnPause.addEventListener('click', pauseStoper);
+btnStop.addEventListener('click', stopStoper);
+btnArchiv.addEventListener('click', showArchiv);
 btnClear.addEventListener('click', clearStoper);
 btnInfo.addEventListener('click', showInfo);
 btnCloseModal.addEventListener('click', hideInfo);
 btnTheme.addEventListener('click', themePanelDisplay);
-
